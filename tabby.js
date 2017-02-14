1f77dd422c6a7a941538e953ce4bac8bf404c8c9
NickHarvey2 <Nick.Harvey.Developer@gmail.com>
2017-02-14 15:56:38 -0600

    adding jquery

--- tabby.js
+++ tabby.js
@@ -1,9 +1,8 @@
 (function (){
-  var tabDiv = document.createElement('DIV');
-  var categoryDiv = document.createElement('DIV');
 
   document.addEventListener('DOMContentLoaded', function() {
-    var contentPane = document.getElementById('content');
+    var tabDiv = document.getElementById('tabDiv')
+    var categoryDiv = document.getElementById('categoryDiv');
     
     getTabs(function(tabs) {
       renderTabList(tabs, tabDiv);
@@ -12,9 +11,6 @@
     getBookmarks(function(bookmarks) {
       renderCategories(bookmarks, categoryDiv);
     });
-    
-    contentPane.appendChild(tabDiv);
-    contentPane.appendChild(categoryDiv);
   });
 
   function renderTabList(tabs, tabDiv) {
@@ -46,15 +42,31 @@
   }
 
   function getBookmarks(callback) {
+    getRootBookmark(function(rootBookmark) {
+      chrome.bookmarks.getChildren(rootBookmark.id, function(children) {
+        callback(children);
+      });
+    });
+  }
+  
+  function getRootBookmark(callback) {
     chrome.bookmarks.search({
-
+      title: 'Tabby',
+      url: null
     }, function(bookmarks) {
-      callback(bookmarks);
+      if (bookmarks.length > 0) {
+        callback(bookmarks[0]);
+      } else {
+        chrome.bookmarks.create({
+          title: 'Tabby',
+          url: null
+        }, function(rootBookmark) {
+          callback(rootBookmark);
+        });
+      }
     });
   }
 
   function renderCategories(bookmarks, categoryDiv) {
-    for (var bookmarkIdx in bookmarks) {
-    }
   } 
 })();
