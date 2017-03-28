d125d64c4c41ec3947790371ca5fa15bdf77c7af
NickHarvey2 <Nick.Harvey.Developer@gmail.com>
2017-03-28 14:48:44 -0500

    updates to the export feature

--- silver-telegram-menu.js
+++ silver-telegram-menu.js
@@ -10,20 +10,51 @@
   chrome.contextMenus.onClicked.addListener(function(info, tab) {
     if (info.menuItemId === 'fa46ea8e-a358-4b09-be55-901083769c45') {
       chrome.bookmarks.getTree(function(results) {
-        console.log(results.map(bookmarkTreeNodeToXml).join('\n'));
+        let xmlStr = '<!DOCTYPE NETSCAPE-Bookmark-file-1>\n'
+          + '<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">\n'
+          + '<TITLE>Bookmarks</TITLE>\n'
+          + '<H1>Bookmarks</H1>\n'
+          + '<DL><p>'
+          + results[0].children.map(bookmarkTreeNodeToXml, {level: 1}).join('')
+          + '\n</DL><p>\n';
+        let xmlFile = new File([xmlStr], 'bookmarks.html');
+        let xmlFileUrl = URL.createObjectURL(xmlFile);
+        chrome.downloads.download({
+          url: xmlFileUrl,
+          filename: 'bookmarks.html',
+          saveAs: true
+        }, function(downloadId) {
+          URL.revokeObjectURL(xmlFileUrl);
+        });
       });
     }
   });
 
   function bookmarkTreeNodeToXml(bookmarkTreeNode) {
-    let retVal = '<DT>';
+    let indent = Array(this.level).fill('\t\t').join('');
+    let retVal = '\n' + indent + '<DT>';
     if (bookmarkTreeNode.url) {
+      var xhr = new XMLHttpRequest();
+      xhr.responseType = 'blob';
+      xhr.onreadystatechange = function() {
+        if (this.status === 200 && this.readyState === 4) {
+          console.log(this.response);
+          let reader = new FileReader();
+          reader.onloadend = function() {
+            console.log(reader.result);
+          };
+          console.log(this.responseType);
+          reader.readAsDataURL(this.response);
+        }
+      };
+      xhr.open('GET', 'chrome://favicon/' + bookmarkTreeNode.url);
+      xhr.send();
       retVal += '<A HREF="';
       retVal += bookmarkTreeNode.url;
       retVal += '" ADD_DATE="';
       retVal += bookmarkTreeNode.dateAdded;
       retVal += '" ICON="';
-      retVal += ''; // TODO
+      retVal += 'chrome://favicon/' + bookmarkTreeNode.url;
       retVal += '">';
       retVal += bookmarkTreeNode.title;
       retVal += '</A>';
@@ -34,8 +65,9 @@
       retVal += bookmarkTreeNode.dateGroupModified;
       retVal += '">';
       retVal += bookmarkTreeNode.title;
-      retVal += '</H3>\n<DL><p>\n';
-      retVal += bookmarkTreeNode.children.map(bookmarkTreeNodeToXml).join('\n');
+      retVal += '</H3>\n' + indent + '<DL><p>';
+      retVal += bookmarkTreeNode.children.map(bookmarkTreeNodeToXml, {level: this.level + 1}).join('');
+      retVal += '</DL><p>';
     }
     return retVal;
   }
